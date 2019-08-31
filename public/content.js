51f349cf6cd7130138c01773bbc77ae81a146ce3
Kyle Carhart <KMCGamer@live.com>
2019-08-31 10:26:41 -0700

    added comments

--- public/content.js
+++ public/content.js
@@ -3,10 +3,12 @@ const ENTRIES_STORAGE_LOCATION = 'entries'
 chrome.storage.sync.get([ENTRIES_STORAGE_LOCATION], function(result) {
   const entries = result[ENTRIES_STORAGE_LOCATION]
   if (entries) {
+    // Get all links on the page related to a google search
     document.querySelectorAll('.g .r > a').forEach(node => {
       entries.some(entry => {
         if (node.href.includes(entry)) {
-          node.closest('.g').style.display = 'none' // Todo: find a better way to do this
+          // Remove all search results containing blacklisted domains
+          node.closest('.g').style.display = 'none'
         }
       })
     })
