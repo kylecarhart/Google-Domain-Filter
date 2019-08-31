27d0927599c888fd440711768c74f3c33f920530
Kyle Carhart <KMCGamer@live.com>
2019-08-31 08:37:40 -0700

    refined selector and dom traversal

--- public/content.js
+++ public/content.js
@@ -3,10 +3,10 @@ const ENTRIES_STORAGE_LOCATION = 'entries'
 chrome.storage.sync.get([ENTRIES_STORAGE_LOCATION], function(result) {
   const entries = result[ENTRIES_STORAGE_LOCATION]
   if (entries) {
-    document.querySelectorAll('.srg a').forEach(node => {
+    document.querySelectorAll('.g .r > a').forEach(node => {
       entries.some(entry => {
         if (node.href.includes(entry)) {
-          node.parentElement.parentElement.style.display = 'none' // Todo: find a better way to do this
+          node.closest('.g').style.display = 'none' // Todo: find a better way to do this
         }
       })
     })
