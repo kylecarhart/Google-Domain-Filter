34236e28776997772a109297262016457667b2a2
Kyle Carhart <KMCGamer@live.com>
2019-09-06 14:49:40 -0400

    added listener to content script

--- public/content.js
+++ public/content.js
@@ -1,16 +1,40 @@
 const ENTRIES_STORAGE_LOCATION = 'entries'
 
+// When entries are added to storage, modify the DOM
+const changeListener = change => {
+  console.log('storage onchanged')
+  // Check if entries were changed (and not some other part of storage)
+  if (change[ENTRIES_STORAGE_LOCATION]) {
+    modifyDOM(change[ENTRIES_STORAGE_LOCATION].newValue)
+  }
+}
+
+// Remove all search results containing blacklisted domains
+const modifyDOM = entries => {
+  // Get all links on the page related to a google search
+  document.querySelectorAll('.g .r > a').forEach(node => {
+    if (entries.length > 0) {
+      for (let i = 0; i < entries.length; i++) {
+        if (node.href.includes(entries[i])) {
+          node.closest('.g').style.display = 'none'
+          break
+        } else {
+          node.closest('.g').style.display = null
+        }
+      }
+    } else {
+      node.closest('.g').style.display = null
+    }
+  })
+}
+
+// On page load, get entries from storage and modify the DOM
 chrome.storage.sync.get([ENTRIES_STORAGE_LOCATION], function(result) {
   const entries = result[ENTRIES_STORAGE_LOCATION]
   if (entries) {
-    // Get all links on the page related to a google search
-    document.querySelectorAll('.g .r > a').forEach(node => {
-      entries.some(entry => {
-        if (node.href.includes(entry)) {
-          // Remove all search results containing blacklisted domains
-          node.closest('.g').style.display = 'none'
-        }
-      })
-    })
+    modifyDOM(entries)
   }
 })
+
+// Listen for changes to entries
+chrome.storage.onChanged.addListener(changeListener)
