4619e8751de33cf327e0824253e28a71ced45f0f
Kyle Carhart <KMCGamer@live.com>
2019-12-14 10:50:12 -0500

    added validity check for duplicate domains

--- src/popup/App.js
+++ src/popup/App.js
@@ -1,7 +1,6 @@
 /*global chrome*/
 
 import React, { useState } from 'react'
-import './App.css'
 import Table from './components/table/Table'
 import DomainStorageController, {
   DOMAINS_STORAGE_LOCATION
@@ -9,17 +8,18 @@ import DomainStorageController, {
 import InputWithButton from './components/input/InputWithButton'
 import Tip from './components/tip/Tip'
 import Info from './icons/Info'
+import './App.css'
 
 const regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
 
 export default function App() {
-  const [entries, setEntries] = useState([])
+  const [domains, setDomains] = useState([])
   const [selectedEntry, setSelectedEntry] = useState(-1)
 
   const changeListener = change => {
     // Check if entries were changed (and not some other part of storage
     if (change[DOMAINS_STORAGE_LOCATION]) {
-      setEntries(change[DOMAINS_STORAGE_LOCATION].newValue)
+      setDomains(change[DOMAINS_STORAGE_LOCATION].newValue)
     }
   }
 
@@ -29,7 +29,7 @@ export default function App() {
   React.useEffect(() => {
     DomainStorageController.getDomains()
       .then(domains => {
-        setEntries(domains)
+        setDomains(domains)
       })
       .catch(e => {
         console.log(e)
@@ -51,13 +51,15 @@ export default function App() {
         <InputWithButton
           btnClick={input => DomainStorageController.addDomain(input)}
           placeholder="Enter Domains"
-          isValid={input => input && regex.test(input)}
+          isValid={input =>
+            input && regex.test(input) && !domains.includes(input)
+          }
         />
       </div>
       <div className="small-header">Filtered Domains</div>
-      {entries.length > 0 ? (
+      {domains.length > 0 ? (
         <Table
-          entries={entries}
+          entries={domains}
           handleClearClick={DomainStorageController.removeDomain}
           handleEditClick={idx => setSelectedEntry(idx)}
         />
