e56fa86eb45227280b872770c37368031472d71b
Kyle Carhart <KMCGamer@live.com>
2019-12-31 14:20:06 -0500

    fix to hook

--- src/popup/hooks/useOutsideClickHandler.js
+++ src/popup/hooks/useOutsideClickHandler.js
@@ -1,18 +1,18 @@
 import { useEffect } from 'react'
 
 export default function useOutsideClickHandler(ref, isDisabled, callback) {
-  const handleClick = e => {
-    if (!ref.current.contains(e.target) && !isDisabled) {
-      callback()
-    }
-  }
-
   useEffect(() => {
+    const handleClick = e => {
+      if (!ref.current.contains(e.target) && !isDisabled) {
+        callback()
+      }
+    }
+
     if (!isDisabled) {
       document.addEventListener('mousedown', handleClick)
     }
     return () => {
       document.removeEventListener('mousedown', handleClick)
     }
-  }, [isDisabled])
+  }, [ref, isDisabled, callback])
 }
