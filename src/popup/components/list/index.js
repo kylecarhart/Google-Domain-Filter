ef8afb750689e3384d5cd215aa24ce67903045af
Kyle Carhart <KMCGamer@live.com>
2020-09-13 11:18:44 -0400

    remove drag list item

--- src/popup/components/list/index.js
+++ src/popup/components/list/index.js
@@ -1,6 +1,5 @@
 import List from './List';
 import ListItem from './ListItem';
 import ListItemOptions from './ListItemOptions';
-import DraggableListItem from './DraggableListItem';
 
-export { List, ListItem, ListItemOptions, DraggableListItem };
+export { List, ListItem, ListItemOptions };
