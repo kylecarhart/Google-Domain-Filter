8e88f1839db0a4c50aabeeaf484cbc73bc72db0b
Kyle Carhart <KMCGamer@live.com>
2020-12-01 17:19:02 -0500

    changed input to DomainInput

--- src/popup/pages/ListPage.jsx
+++ src/popup/pages/ListPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStorage } from "../hooks";
-import { DomainInputBar } from "../components/input";
+import { DomainInputBar } from "../components/DomainInput";
 import DomainList from "../components/DomainList";
 import styled from "styled-components";
 
