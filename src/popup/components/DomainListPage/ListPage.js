92c7eade7bb94f7c56bf89fa5bcb6aa2d1234b64
Kyle Carhart <KMCGamer@live.com>
2020-12-17 21:40:38 -0500

    added message for initial empty list

--- src/popup/components/DomainListPage/ListPage.js
+++ src/popup/components/DomainListPage/ListPage.js
@@ -11,7 +11,11 @@ function ListPage({ storageKey }) {
   return (
     <Page>
       <DomainInputBar domains={domains} setDomains={setDomains} />
-      <DomainList domains={domains} setDomains={setDomains} />
+      {domains.length > 0 ? (
+        <DomainList domains={domains} setDomains={setDomains} />
+      ) : (
+        <EmptyList>Add a domain to get started...</EmptyList>
+      )}
     </Page>
   );
 }
@@ -20,6 +24,20 @@ const Page = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+`;
+
+const EmptyList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #333;
+  font-size: 14px;
+  padding: 0;
+  margin: 8px 0px 0px 0px;
+  border-radius: 3px;
+  box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.05), 0px 1px 2px rgba(0, 0, 0, 0.1);
+  flex: 1 1 auto;
+  height: 0px;
 `;
 
 ListPage.propTypes = {
