271fcacfb890cc7385af4264a9616a8a202b175f
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2021-06-22 18:06:23 -0400

    installed jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      // ...
+      diagnostics: {
+        ignoreCodes: [151001],
+      },
+    },
+  },
+};
