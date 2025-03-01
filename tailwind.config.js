6bf41e18f64768ff14e650b1b9b3047407f4e4d7
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2025-03-01 09:10:14 -0500

    move everything to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./entrypoints/**/*.{html,ts,tsx}"],
+  content: ["./src/entrypoints/popup/**/*.{html,ts,tsx}"],
   theme: {
     extend: {},
   },
--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: 'src',
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
 });
